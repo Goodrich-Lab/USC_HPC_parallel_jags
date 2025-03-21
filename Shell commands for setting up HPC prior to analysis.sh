38c36a61848990d7426bf82c390283c7a9a957ed
hehampson <112519357+hehampson@users.noreply.github.com>
2025-03-21 08:29:47 -0700

    Update Shell commands for setting up HPC prior to analysis.sh

--- Shell commands for setting up HPC prior to analysis.sh
+++ Shell commands for setting up HPC prior to analysis.sh
@@ -9,8 +9,8 @@ ssh jagoodri@endeavour.usc.edu
 
 # TO SOLVE ERROR WHERE JAGS IS NOT RECOGNIZED ON WORKER NODES:
 # (this only needs to be run once)
-module load gcc/11.2.0 patchelf
-patchelf --set-rpath /spack/apps2/linux-centos7-x86_64/gcc-11.2.0/jags-4.3.0-d373ytkqeamusbww7n2qjxyfwgikw2w5/lib /home1/jagoodri/R/x86_64-pc-linux-gnu-library/4.1/rjags/libs/rjags.so
+module load gcc/13.3.0 patchelf
+patchelf --set-rpath /spack/apps2/linux-centos7-x86_64/gcc-13.3.0/jags-4.3.0-d373ytkqeamusbww7n2qjxyfwgikw2w5/lib /home1/jagoodri/R/x86_64-pc-linux-gnu-library/4.1/rjags/libs/rjags.so
 
 
 
@@ -18,8 +18,8 @@ patchelf --set-rpath /spack/apps2/linux-centos7-x86_64/gcc-11.2.0/jags-4.3.0-d37
 module spider jags
 module load usc r
 module load jags
-module load gcc/11.2.0
-module load openblas/0.3.18
+module load gcc/13.3.0
+module load openblas/0.3.28
 
 
 export PKG_CONFIG_PATH=/packages/jags/4.3.0/lib/pkgconfig
